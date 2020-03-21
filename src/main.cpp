066e2987ec59db8a1f900c288d1b28f447abdb7d
StefanYohansson <sy.fen0@gmail.com>
2020-03-21 18:01:27 +0000

    Add app icon

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    const auto appIcon = QIcon("data/ktelephone.png");
+    app.setWindowIcon(appIcon);
     KTelephoneManager m;
     return app.exec();
 }
-
