b16c837f50b6c4ad16e89770613dc84102787646
StefanYohansson <stefan@telnyx.com>
2021-04-28 00:07:27 +0100

    Add new preferences to telephone struct

--- src/gui/ktelephonemanager.hpp
+++ src/gui/ktelephonemanager.hpp
@@ -27,6 +27,21 @@ struct Telephone {
 	QString username;
 	QString password;
 	int active;
+
+	/* prefs */
+	int should_register_startup;
+	int should_subscribe_presence;
+	int should_publish_presence;
+	int should_use_blf;
+	int should_disable_ringback_tone;
+	QString custom_ringtone;
+
+	int transport;
+	int subscription_expiry_delay;
+	int keep_alive_expiry_delay;
+	int registration_expiry_delay;
+	int use_stun;
+	QString stun_server;
 };
 
 typedef struct Telephone Telephone_t;
